d4063ca853a1d7e6e828491fb3001ca1c410b9cc
p3t3c <petec33@gmail.com>
2019-05-27 22:25:20 +1000

    Added comment to explain why I wrote this.

--- going_east_or_south.py
+++ going_east_or_south.py
@@ -1,5 +1,8 @@
 from pygeodesy.ellipsoidalVincenty import LatLon
 
+# This script outputs three points. an origin point a point bearing east and a point bearing south.
+# I wrote this to remind myself that going north/south will not affect the longitude.
+
 BEARING_SOUTH = 180.0
 BEARING_EAST = 90.0
 
